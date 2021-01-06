ffb92b1b12a3a2ec9fc50403aac04cb8f24b2712
gmono <gaozijian0411@foxmail.com>
2021-01-06 17:51:17 +0800

    添加基本架构

--- install.ps1
+++
@@ -1,5 +0,0 @@
-Remove-Item -r .\.git 
-yarn
-git init
-git add .
-git commit -m "first"
--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,57 @@
-export {};
+import React, { ComponentProps, CSSProperties, ReactNode } from "react";
+
+
+
+
+interface SceneProps {
+  FrontLayer: any;
+  Content: any;
+  BackLayer: any;
+  style: CSSProperties;
+  className: string;
+}
+function Scene(props: SceneProps) {
+
+  let css = {
+    position: "absolute",
+    top: 0,
+    left: 0
+  } as CSSProperties;
+  
+  let frontLayer = (
+    <div style={{
+      pointerEvents: "none",
+      ...css
+    }}>
+      {props.FrontLayer}
+    </div>
+  )
+  let content = (
+    <div style={{
+      ...css
+    }
+    }>
+      {props.Content}
+    </div>
+  )
+  let backLayer = (
+    <div style={{
+      pointerEvents: "none",
+      ...css
+    }}>
+      {props.BackLayer}
+    </div>
+  )
+  return (
+    <div style={props.style} className={props.className}>
+        <div style={{position:"relative"}}>
+          {backLayer}
+          {content}
+          {frontLayer}
+        </div>
+    </div>
+    
+  )
+}
+import ReactDOM from "react-dom"
+ReactDOM.render(<Scene BackLayer={<div>fasdfasdf</div>} Content={<div>打发斯蒂芬</div>} FrontLayer={<div>adfadfas</div>} />, document.getElementsByTagName("body")[0])
